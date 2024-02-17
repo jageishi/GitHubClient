4e62f70d0d65ab18030230589fa1e7c085d85dd9
JunAgeishi <junageishi@gmail.com>
2024-02-17 16:05:51 +0900

    ホーム画面をデフォルト表示

--- app/src/main/java/org/ageage/githubclient/MainActivity.kt
+++ app/src/main/java/org/ageage/githubclient/MainActivity.kt
@@ -3,44 +3,15 @@ package org.ageage.githubclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import org.ageage.githubclient.core.ui.theme.GitHubClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            org.ageage.githubclient.core.ui.theme.GitHubClientTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            GitHubClientTheme {
+                GitHubClientNavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GitHubClientTheme {
-        Greeting("Android")
     }
 }
--- feature/home/src/main/java/org/ageage/githubclient/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/org/ageage/githubclient/feature/home/HomeScreen.kt
@@ -1,15 +1,20 @@
 package org.ageage.githubclient.feature.home
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen() {
-    Text(
-        text = "Home Screen",
-    )
+    Scaffold { padding ->
+        Text(
+            modifier = Modifier.padding(padding),
+            text = "Home Screen"
+        )
+    }
 }
 
 @Preview
